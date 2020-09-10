e51b755a7cfbdb521a3b47caed59ab35dd107b4a
Marcos Souza <bytecore@gmail.com>
2020-09-10 00:04:00 -0300

    configuracao do CORS

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
